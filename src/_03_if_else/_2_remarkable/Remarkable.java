02aa9fc6ce65656d892b005563d97d9e1a1d0ff7
root <root@e2147845d2c2>
2020-05-24 00:29:55 +0000

    Just started Venn Diagram.

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
 		// 1. Save something remarkable about each person in a variable.
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+ 
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
 	}
